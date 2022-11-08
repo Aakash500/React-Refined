ebf27e798db4df28622c35c6f23450ffc4d99be7
Aakash Mehta <aakashmehta880@gmail.com>
2022-11-08 21:00:30 +0530

    props-example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Greet from './Components/Greet'
 
 const App = () => {
   return (
     <>
     <h1>Hello World</h1>
+    <Greet name="Aakash"/>
+    <Greet>
+      <p>I study in DTU</p>
+    </Greet>
     </>
   )
 }
